543c5369cfe4fdb03b84e47891c52ddb38775aea
CalebNeal07 <caleboneal07@gmail.com>
2023-10-23 10:49:08 -0400

    Switched VictorSPX to CANSparkMAx

--- src/main/java/com/koibots/robot/Robot.java
+++ src/main/java/com/koibots/robot/Robot.java
@@ -1,60 +1,129 @@
-// Copyright (c) FIRST and other WPILib contributors.
-
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 package com.koibots.robot;
 
-import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.ctre.phoenix.motorcontrol.can.VictorSPXConfiguration;
-import com.ctre.phoenix.motorcontrol.can.VictorSPXPIDSetConfiguration;
-import edu.wpi.first.hal.DriverStationJNI;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.util.WPIUtilJNI;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.internal.DriverStationModeThread;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 /**
- * The VM is configured to automatically run this class. If you change the name of this class or the
- * package after creating this project, you must also update the build.gradle file in the project.
+ * This is a demo program showing the use of the RobotDrive class, specifically
+ * it contains the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
-    VictorSPX motor1;
-    VictorSPX motor2;
-    VictorSPXConfiguration motorConfig = new VictorSPXConfiguration();
+    private Joystick m_stick;
+    private CANSparkMax motor1;
+    private CANSparkMax motor2;
+    private SparkMaxPIDController pidController;
+    private RelativeEncoder encoder;
+    public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
 
-    NetworkTableInstance nt = NetworkTableInstance.getDefault();
-    NetworkTable table;
-
-    final double G_fps = 32.1741;
-    final double theta_rad = StrictMath.PI / 4;
+    @Override
     public void robotInit() {
-        motor1 = new VictorSPX(0);
-        motor2 = new VictorSPX(1);
+        m_stick = new Joystick(0);
 
-        motor2.follow(motor1);
+        // initialize motor
+        motor1 = new CANSparkMax(1, MotorType.kBrushed);
+        motor2 = new CANSparkMax(2, MotorType.kBrushed);
 
-        table = nt.getTable("Inputs");
+        motor2.follow(motor1, false);
+
+
+        /**
+         * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+         * in the SPARK MAX to their factory default state. If no argument is passed, these
+         * parameters will not persist between power cycles
+         */
+        motor1.restoreFactoryDefaults();
+
+        /**
+         * In order to use PID functionality for a controller, a SparkMaxPIDController object
+         * is constructed by calling the getPIDController() method on an existing
+         * CANSparkMax object
+         */
+        pidController = motor1.getPIDController();
+
+        // Encoder object created to display position values
+        encoder = motor1.getEncoder();
+
+        // PID coefficients
+        kP = 6e-5;
+        kI = 0;
+        kD = 0;
+        kIz = 0;
+        kFF = 0.000015;
+        kMaxOutput = 1;
+        kMinOutput = -1;
+        maxRPM = 4200;
+
+        // set PID coefficients
+        pidController.setP(kP);
+        pidController.setI(kI);
+        pidController.setD(kD);
+        pidController.setIZone(kIz);
+        pidController.setFF(kFF);
+        pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+        // display PID coefficients on SmartDashboard
+        SmartDashboard.putNumber("P Gain", kP);
+        SmartDashboard.putNumber("I Gain", kI);
+        SmartDashboard.putNumber("D Gain", kD);
+        SmartDashboard.putNumber("I Zone", kIz);
+        SmartDashboard.putNumber("Feed Forward", kFF);
+        SmartDashboard.putNumber("Max Output", kMaxOutput);
+        SmartDashboard.putNumber("Min Output", kMinOutput);
     }
-    
 
-    public void disabled() {
-        motor1.set(VictorSPXControlMode.PercentOutput, 0);
-    }
+    @Override
+    public void teleopPeriodic() {
+        // read PID coefficients from SmartDashboard
+        double p = SmartDashboard.getNumber("P Gain", 0);
+        double i = SmartDashboard.getNumber("I Gain", 0);
+        double d = SmartDashboard.getNumber("D Gain", 0);
+        double iz = SmartDashboard.getNumber("I Zone", 0);
+        double ff = SmartDashboard.getNumber("Feed Forward", 0);
+        double max = SmartDashboard.getNumber("Max Output", 0);
+        double min = SmartDashboard.getNumber("Min Output", 0);
 
+        // if PID coefficients on SmartDashboard have changed, write new values to controller
+        if((p != kP)) { pidController.setP(p); kP = p; }
+        if((i != kI)) { pidController.setI(i); kI = i; }
+        if((d != kD)) { pidController.setD(d); kD = d; }
+        if((iz != kIz)) { pidController.setIZone(iz); kIz = iz; }
+        if((ff != kFF)) { pidController.setFF(ff); kFF = ff; }
+        if((max != kMaxOutput) || (min != kMinOutput)) {
+            pidController.setOutputRange(min, max);
+            kMinOutput = min; kMaxOutput = max;
+        }
 
-    public void autonomous() {
-        double height = table.getEntry("Height").getDouble(0);
-        double targetDistance = table.getEntry("Distance").getDouble(4);
+        /**
+         * PIDController objects are commanded to a set point using the
+         * SetReference() method.
+         *
+         * The first parameter is the value of the set point, whose units vary
+         * depending on the control type set in the second parameter.
+         *
+         * The second parameter is the control type can be set to one of four
+         * parameters:
+         *  com.revrobotics.CANSparkMax.ControlType.kDutyCycle
+         *  com.revrobotics.CANSparkMax.ControlType.kPosition
+         *  com.revrobotics.CANSparkMax.ControlType.kVelocity
+         *  com.revrobotics.CANSparkMax.ControlType.kVoltage
+         */
+        double setPoint = m_stick.getY()*maxRPM;
+        pidController.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
 
-        double flywheelVelocity = StrictMath.sqrt((G_fps * targetDistance) / (StrictMath.sin(2 * theta_rad)));
-
-        motor1.set(VictorSPXControlMode.Velocity, flywheelVelocity);
+        SmartDashboard.putNumber("SetPoint", setPoint);
+        SmartDashboard.putNumber("ProcessVariable", encoder.getVelocity());
     }
 }
